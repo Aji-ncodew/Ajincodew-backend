d912ca76dce0326f2213c037781eec9d9f5fb444
TAFFAHACHRAF <a.taffah@etu.enset-media.ac.ma>
2024-01-19 17:30:20 +0100

    solve vercel config

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-// routes/index.js
 const express = require('express');
 const router = express.Router();
-const blogRoutes = require('./blogRoutes');
+const blogRoutes = require('./routes/blogRoutes');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 5050;
+const routes = require('./routes/blogRoutes');
 
 router.use('/', blogRoutes);
 
-module.exports = router;
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/', routes);
+
+app.get('/', (req, res) => {
+    res.send('This is my demo project');
+});
+
+app.listen(PORT, function () {
+    console.log('Demo project at: ' + PORT);
+});
