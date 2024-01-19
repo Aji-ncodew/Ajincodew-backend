33f1b3533b242de520ae01bba6ae3cbe97980a2b
TAFFAHACHRAF <a.taffah@etu.enset-media.ac.ma>
2024-01-19 14:30:34 +0100

    cosum vercel api with a simple client

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
 const PORT = process.env.PORT || 5050
-const { books } = require('./books.js')
+const { books } = require('./books.js');
+
+// Enable CORS for all routes
+app.use(cors());
 
 app.get('/books', books);
+
 app.get('/', (req, res) => {
-    res.send('This is my demo project')
-})
+    res.send('This is my demo project');
+});
 
 app.listen(PORT, function () {
-console. log('Demo project at: '+PORT) });
-
-
-
-
-
-
-
-
+    console.log('Demo project at: ' + PORT);
+});
