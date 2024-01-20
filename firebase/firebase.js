860522ab06a2f95caa0ff954ac1b1509c0513975
TAFFAHACHRAF <a.taffah@etu.enset-media.ac.ma>
2024-01-20 03:49:43 +0100

    secure firebase data access

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -3,19 +3,27 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+require('dotenv').config();
+const apiKey = process.env.apiKey;
+const authDomain = process.env.authDomain;
+const projectId = process.env.projectId;
+const storageBucket = process.env.storageBucket;
+const messagingSenderId = process.env.messagingSenderId;
+const appId = process.env.appId;
+const measurementId = process.env.measurementId;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCXu3gcyDQFKnpIXqLuOEtcMcN4G6HNPiA",
-  authDomain: "ajincodew-a61d8.firebaseapp.com",
-  projectId: "ajincodew-a61d8",
-  storageBucket: "ajincodew-a61d8.appspot.com",
-  messagingSenderId: "1057022551227",
-  appId: "1:1057022551227:web:bc50156494feff970d359e",
-  measurementId: "G-87W1H8Z593"
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId
 };
 
 // Initialize Firebase
@@ -24,4 +32,4 @@ const analytics = getAnalytics(app);
 
 
 firebase.initializeApp(firebaseConfig); //initialize firebase app 
-module.exports = { firebase }; //export the app
+module.exports = { firebase,analytics }; //export the app
