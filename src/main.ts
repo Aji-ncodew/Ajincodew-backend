82926e85b76ded515bbf25f5632573e3fb209285
TAFFAHACHRAF <a.taffah@etu.enset-media.ac.ma>
2024-01-22 04:50:53 +0100

    add default endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { BlogModule } from './blog/blog.module';
+import { AppModule } from './app.module';
 require('dotenv').config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(BlogModule);
-  const port = process.env.PORT || 3000; 
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3001; 
   console.log("Server running on : "+process.env.PORT)
   await app.listen(port);
 }
