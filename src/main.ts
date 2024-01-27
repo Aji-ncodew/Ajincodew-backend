c5237f5b568eb4718b7b1895d138e2220294bdd4
TAFFAHACHRAF <a.taffah@etu.enset-media.ac.ma>
2024-01-28 00:29:29 +0100

    dockerize

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { BlogModule } from './blog/blog.module';
 import { AppModule } from './app.module';
 require('dotenv').config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(BlogModule);
+  const app = await NestFactory.create(AppModule);
   const port = 3088; 
   console.log("Server running on : "+process.env.PORT)
   await app.listen(port);
