cfb8d6190cdb86d3467482bee8d5fff3602eef95
TAFFAHACHRAF <a.taffah@etu.enset-media.ac.ma>
2024-01-28 00:45:32 +0100

    new build

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { BlogModule } from './blog/blog.module';
-import { AppModule } from './app.module';
+import { BlogModule } from './blog.module';
 require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(BlogModule);
-  const port = 3088; 
+  const port = process.env.PORT || 80; 
   console.log("Server running on : "+process.env.PORT)
   await app.listen(port);
 }
