8d1760c317a169eca8819c0e474a79688f6fe3da
TAFFAHACHRAF <a.taffah@etu.enset-media.ac.ma>
2024-02-08 00:20:17 +0100

    bycrypt the password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,9 +2,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/user/user.model';
 import { UsersService } from 'src/user/users.service';
-import * as dotenv from 'dotenv';
-
-dotenv.config(); // Load environment variables from .env file
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -15,25 +13,28 @@ export class AuthService {
     
   async validateUser(email: string, password: string): Promise<User> {
     const user = await this.usersService.getUserByEmail(email);
-    if (user && user.password === password) {
+    if (user && await bcrypt.compare(password, user.password)) {
       return user;
     }
     return null;
   }
   
   async login(username: string, password: string) {
-  const user = await this.validateUser(username, password);
-  if (!user) {
-    throw new UnauthorizedException('Invalid credentials');
+    const user = await this.validateUser(username, password);
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    const payload = { username: user.username, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload, { secret: 'zezr8f54zef8zeaepoijfirjzfef7877z89ref456zerf8ez54rf89e4fze5f4' }),
+    };
   }
-  const payload = { username: user.username, sub: user.id };
-  return {
-    access_token: this.jwtService.sign(payload, { secret: 'zezr8f54zef8zeaepoijfirjzfef7877z89ref456zerf8ez54rf89e4fze5f4' }),
-  };
-}
-
 
   async signUp(user: User) {
+    // Hash the password before saving
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+    user.password = hashedPassword;
+
     // Assign admin role to the user
     user.roles = ['admin'];
 
